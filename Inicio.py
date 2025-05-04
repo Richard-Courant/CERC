52ea4a558c72c3d34394997bc27b022f568083ba
Richard-Courant <realidad.alostatica@gmail.com>
2025-05-04 21:18:46 +0000

    ok

--- Inicio.py
+++ Inicio.py
@@ -5,6 +5,7 @@ import streamlit as st
 from modules.Cotizador import Cotizador
 from PIL import Image
 from routes.mensaje_rcourant_whatsapp import  whatsapp_rcourant
+import os 
 
 # Configuración de la página
 st.set_page_config(
@@ -38,5 +39,9 @@ Buscamos proporcionar un acompañamiento académico integral, personalizado y ac
 # Enviar mensaje a Richar Courant 
 mensaje = "¡Hola! Quiero pedir información sobre los servicios de Richard Courant."
 whatsapp_rcourant(mensaje=mensaje)
-imagen_base = Image.open("/workspaces/CERC/assets/cerc_rcourant_base.png")
-st.image(imagen_base)
+
+filepath_base = r"\workspaces\CERC\assets\cerc_rcourant_base.png"
+
+if os.path.exists(filepath_base): 
+    imagen_base = Image.open(filepath_base)
+    st.image(imagen_base)
